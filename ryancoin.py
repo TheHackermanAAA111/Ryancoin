faa2411f3700feed4aade283c1d36389d93d6c5a
IAmRyanHe <>
2021-07-16 23:30:45 +0000

    we did stuff that will call in the cops

--- ryancoin.py
+++ ryancoin.py
@@ -42,14 +42,40 @@ class Blockchain:
 	@staticmethod
   def check_validity():
 		#check whether the blockchain is vaild
-	pass
+    if prev_block.index + 1 != block.index:
+      return False
+    elif prev_block.calculate_hash != block.prev_hash:
+      return False
+    elif not Blockchain.verifiying_proof(block.proof_no, prev_block.proof_no):
+      return False
+    elif block.timestamp <= prev_block.timestamp:
+      return False
+
+    #passed all tests HOORAY
+    return True
   def new_data(self, sender, recipient, quantity):
 		#adds a new transaction to the data of the transactions
-	pass
+    self.current_data.append({
+      'sender': sender,
+      'recipient': recipient,
+      'quantity': quantity
+    })
+    return True
+	
   @staticmethod
   def construct_proof_of_work(prev_proof):
 		#protects the blockchain from attack
-	pass
+    proof_no = 0
+    while Blockchain.verifying_proof(proof_no,prev_proof) is false:
+      proof_no += 1 
+    return proof_no
+  
+  @staticmethod
+  def verifying_proof(prev_proof, proof):
+    #does hash (last_proof, proof) contain 4 leading 0s
+    guess = f'{prev_proof}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexidigest()
+    return guess_hash[:4] == "0000"
   @property
   def last_block(self):
 		#returns the last block of the blockchain

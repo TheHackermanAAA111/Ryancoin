4ab91be550609d7af56d0625a09973c57b175b9b
D3vanagar1 <755980@gmail.com>
2021-07-30 15:50:01 -0700

    uncommented check_validity

--- ryancoin.py
+++ ryancoin.py
@@ -49,19 +49,19 @@ class Blockchain:
         return True
 
 
-    #@staticmethod
-    #def check_validity():
-        ##check whether the blockchain is valid
-        #if prev_block.index +1 != block.index:
-            #return False
-        #elif prev_block.calculate_hash != block.prev_hash:
-            #return False
-        #elif not Blockchain.verifying_proof(block.proof_no,prev_block.proof_no):
-            #return False
-        #elif block.timestamp <= prev_block.timestamp:
-            #return False
-        ##passed all tests HOORAY
-        #return True
+    @staticmethod
+    def check_validity():
+        #check whether the blockchain is valid
+        if prev_block.index +1 != block.index:
+            return False
+        elif prev_block.calculate_hash != block.prev_hash:
+            return False
+        elif not Blockchain.verifying_proof(block.proof_no,prev_block.proof_no):
+            return False
+        elif block.timestamp <= prev_block.timestamp:
+            return False
+        #passed all tests HOORAY
+        return True
 
     def new_data(self, sender, recipient, quantity):
         #adds a new transaction to the data of the transactions

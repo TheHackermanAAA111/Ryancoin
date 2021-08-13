2dadb3a51d91f33310fe5e3bcb5d023f1dab6461
D3vanagar1 <755980@gmail.com>
2021-08-13 15:42:15 -0700

    Finished client class

--- client.py
+++ client.py
@@ -1,3 +1,19 @@
 # hold client class
 
 
+import Crypto
+from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+
+
+class Client:
+    def __init__(self):
+        random = Crypto.Random.new().read
+        self._private_key = RSA.generate(1024,random)
+        self._public_key = self._private_key.publickey()
+        self._signer = PKCS1_v1_5.new(self._private_key)
+
+    @property
+    def identity(self):
+        return binascii.hexlify(self._public_key.exportKey(format= 'DER')).decode('ascii')

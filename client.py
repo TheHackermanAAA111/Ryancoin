2837c3b932b83eef6c8d74067dc797bc798323ef
D3vanagar1 <755980@gmail.com>
2021-08-20 16:50:36 -0700

    Added some notes

--- client.py
+++ client.py
@@ -1,15 +1,15 @@
 # hold client class
 
 
-import Crypto
-from Crypto.Hash import SHA
-from Crypto.PublicKey import RSA
-from Crypto.Signature import PKCS1_v1_5
+import crypto
+from crypto.Hash import SHA
+from crypto.PublicKey import RSA
+from crypto.Signature import PKCS1_v1_5
 
 
 class Client:
     def __init__(self):
-        random = Crypto.Random.new().read
+        random = crypto.Random.new().read
         self._private_key = RSA.generate(1024,random)
         self._public_key = self._private_key.publickey()
         self._signer = PKCS1_v1_5.new(self._private_key)
@@ -17,3 +17,23 @@ class Client:
     @property
     def identity(self):
         return binascii.hexlify(self._public_key.exportKey(format= 'DER')).decode('ascii')
+
+
+Bob = Client()
+print(Bob.identity)
+
+'''
+
+Bob.identity = 0929209235125321171272...2123124124
+Jack = Client()
+
+t = Transaction(
+        Bob,
+        Jack.identity,
+        100.0
+        )
+signature = t.sign_transaction() = 7ce371wqo4euquqweyey...972
+
+transactions = [].append(t)
+
+'''

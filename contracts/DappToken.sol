3b0bb2ae7956bb35d72921499b52a18cc841256d
D3vanagar1 <755980@gmail.com>
2021-09-03 16:59:10 -0700

    setup code written for dapptoken

---
+++ contracts/DappToken.sol
@@ -0,0 +1,17 @@
+// declare which version of solidity we are using
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DappToken{
+
+    // constructor
+    // set the total number of tokens
+    // read the total number of tokens
+
+    // global var that follows erc-20 standard
+    uint256 public totalSupply // type visibility name
+    function DappToken () public {
+        totalSupply = 1000000;
+    }
+
+
+}

6d1e0855ac2a5c9c37daaab2ea45db791b90144a
Ryan <Ryan@RyanHEMacBookAir2020.local>
2021-11-05 16:33:51 -0700

    Bug fixes for facilitates token buying and because somebody needed it

--- test/DappTokenSale.js
+++ test/DappTokenSale.js
@@ -55,7 +55,7 @@ contract('DappTokenSale', function(accounts) {
             // attempt to buy tokens different from ether value
             return tokenSaleInstance.buyTokens(numberOfTokens, {from:buyer, value:1});
         }).then(assert.fail).catch(function(error) {
-            ssert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei');
             return tokenSaleInstance.buyTokens(800000, { from: buyer, value: numberOfTokens * tokenPrice })
         }).then(assert.fail).catch(function(error) {
             assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than available');

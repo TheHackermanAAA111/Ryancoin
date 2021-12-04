0a4cfd65b8ac7f948859d0fccca01eaab051423c
D3vanagar1 <755980@gmail.com>
2021-12-03 17:00:17 -0800

    Front End of app.js finished. Completed event handling and rendering (loading).

--- src/js/app.js
+++ src/js/app.js
@@ -48,11 +48,89 @@ App = {
     },
 
 
-    // listenForEvents
-    // render
+    // listenForEvents - those emitted by contracts
+    listenForEvents: function(){
+        App.contracts.DappTokenSale.deployed().then(function(instance) {
+            instance.Sell({}, {
+                fromBlock: 0,
+                toBlock, 'latest',
+            }).watch(function(error, event) {
+                console.log("event triggered", event);
+                App.render();
+            })
+        })
+    },
 
+    // render
+    render: function(){
+        if(App.loading){
+            return;
+        }
+        App.loading = true;
+
+        var loader = $('#loader');
+        var content = $('#content');
+
+        loader.show();
+        content.hide();
+
+        // load account data
+        web3.eth.getCoinbase(function(err, account){
+            if(err == null){
+                App.account = account;
+                $('#accountAdress').html("Your Account: " + account);
+            }
+        })
+
+        // load the token sale contract
+        App.contracts.DappTokenSale.deployed().then(function(instance) {
+            ryanCoinInstance = instance;
+            return ryanCoinInstance.tokenPrice();
+        }).then(function(tokenPrice) {
+            App.tokenPrice = tokenPrice;
+            $('.token-price').html(web3.fromWei(App.tokenPrice, "ether").toNumber());
+            return ryanCoinInstance.tokensSold();
+        }).then(function(tokensSold) {
+            App.tokensSold = tokensSold.toNumber();
+            $('.token-sold').html(App.tokensSold);
+            $('.tokens-available').html(App.tokensAvailable);
+
+            var progressPercent = (Math.ceil(App.tokensSold) / App.tokensAvailable) * 100;
+            $('#progress').css('width', progressPercent + '%')
+
+            //load token contract
+            App.contracts.DappToken.deployed().then(function(instance) {
+                ryanCoinInstance = instance;
+                return ryanCoinInstance.balanceOf(App.account);
+            }).then(function(balance){
+                $('.ryan-balance').html(balance.toNumber());
+                App.loading = false;
+                loader.hide();
+                content.show();
+            })
+        });
+    },
+
+    buyTokens: function() {
+        $('#content').hide();
+        $('#loader').show();
+        var numberOfTokens = $('#numberOfTokens').val();
+        App.contracts.DappTokenSale.deployed().then(function(instance){
+            return instance.buyTokens(numberOfTokens, {
+                from: App.account,
+                value: numberOfTokens * App.tokenPrice,
+                gas: 500000 // Gas limit
+            });
+        }).then(function(result){
+            console.log("Tokens bought ...")
+            $('from').trigger('reset') // reset number of tokens in form
+            // Wait to Sell event
+        });
+    }
 }
 
-// load token sale contract
-
-
+$(function() {
+    $(window).load(function() {
+        App.init();
+    })
+});
